28dd6250c7786766b330e8b864e6867d5a9fcbc4
Fábio Maia <fabiu.maia@gmail.com>
2017-12-06 20:15:11 +0000

    Refactor

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,25 @@
 using namespace cv;
 using namespace std;
 
+vector<Point2f> getBoardCorners(Mat frame)
+{
+    Size patternsize(7, 7); //interior number of corners
+    vector<Point2f> corners; //this will be filled by the detected corners
+    bool patternfound = findChessboardCorners(frame, patternsize, corners, CALIB_CB_ADAPTIVE_THRESH + CALIB_CB_NORMALIZE_IMAGE + CALIB_CB_FAST_CHECK);
+    if(patternfound) {
+        // improve accuracy
+        cornerSubPix(frame, corners, Size(11, 11), Size(-1, -1), TermCriteria(CV_TERMCRIT_EPS + CV_TERMCRIT_ITER, 30, 0.1));
+
+        cout << endl;
+        for (std::vector<Point2f>::const_iterator i = corners.begin(); i != corners.end(); ++i)
+            cout << *i << ", ";
+        cout << endl;
+
+        return corners;
+    }
+    return vector<Point2f>();
+}
+
 int main(int argc, char* argv[])
 {
     VideoCapture cap;
@@ -24,27 +43,16 @@ int main(int argc, char* argv[])
 
     namedWindow("edges", 1);
     namedWindow("corners", 1);
-    while(true)
-    {
+    while(true) {
         Mat frame;
         cap >> frame; // get a new frame from camera
 
         Mat gray;
         cvtColor(frame, gray, CV_BGR2GRAY);
 
-        Size patternsize(7, 7); //interior number of corners
-        vector<Point2f> corners; //this will be filled by the detected corners
-        bool patternfound = findChessboardCorners(frame, patternsize, corners, CALIB_CB_ADAPTIVE_THRESH + CALIB_CB_NORMALIZE_IMAGE + CALIB_CB_FAST_CHECK);
-        if(patternfound) {
-            cout << endl;
-            cornerSubPix(gray, corners, Size(11, 11), Size(-1, -1), TermCriteria(CV_TERMCRIT_EPS + CV_TERMCRIT_ITER, 30, 0.1));
-            for (std::vector<Point2f>::const_iterator i = corners.begin(); i != corners.end(); ++i)
-                cout << *i << ", ";
-            cout << endl;
-        }
-
         Mat img = gray.clone();
-        drawChessboardCorners(img, patternsize, Mat(corners), patternfound);
+        vector<Point2f> corners = getBoardCorners(gray);
+        drawChessboardCorners(img, Size(7, 7), Mat(corners), !corners.empty());
 
         Mat edges;
         GaussianBlur(gray, edges, Size(7, 7), 1.5, 1.5);
