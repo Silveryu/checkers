4b718fb57395a512606d4ec2d005685f6f6ee8f4
Fábio Maia <fabiu.maia@gmail.com>
2018-01-07 17:32:49 +0000

    Refactor and fix stupid shit

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,6 @@
 #include <iostream>
 #include "game.hpp"
 
-#define STRIDE 8
-
 void tile(const std::vector<cv::Mat> &src, cv::Mat &dst, int grid_x, int grid_y)
 {
     // patch size
@@ -21,11 +19,59 @@ void tile(const std::vector<cv::Mat> &src, cv::Mat &dst, int grid_x, int grid_y)
     }
 }
 
+std::vector<cv::Point2f> completeBoardCorners(std::vector<cv::Point2f> insideCorners)
+{
+    cv::Size patternsize(7, 7);
+    int insideCornerDim = patternsize.width;
+
+    cv::Point2f vertex1 = cv::Point2f(
+        insideCorners[0].x + abs(insideCorners[0].x - insideCorners[insideCornerDim].x),
+        insideCorners[0].y - abs(insideCorners[0].y - insideCorners[1].y)
+    );
+
+    cv::Point2f vertex2 = cv::Point2f(
+        insideCorners[insideCorners.size()-1].x - abs(insideCorners[insideCorners.size()-1].x - insideCorners[insideCorners.size()-1-insideCornerDim].x),
+        insideCorners[insideCorners.size()-1].y + abs(insideCorners[insideCorners.size()-1].y - insideCorners[insideCorners.size()-2].y)
+    );
+
+    int cornerDim = insideCornerDim+2;
+    std::vector<cv::Point2f> corners = std::vector<cv::Point2f>(cornerDim*cornerDim);
+
+    // first column
+    corners[0] = vertex1;
+    for(int i = 0; i < insideCornerDim; ++i){
+        corners[i+1] =  cv::Point2f(vertex1.x, insideCorners[i].y);
+    }
+    corners[cornerDim-1] = cv::Point2f(vertex1.x, vertex2.y);
+
+    // for every insideCorners' x coord
+    for(int i = 1; i < cornerDim -1 ; ++i){
+        int xCoord = insideCorners[(i-1)*insideCornerDim].x;
+        corners[i*cornerDim] = cv::Point2f(xCoord, vertex1.y);
+
+        //for every Corners' y coord
+        for(int j = 0;j < insideCornerDim;j++){
+            corners[i*cornerDim+j+1] = cv::Point2f(xCoord, insideCorners[j].y);
+        }
+
+        corners[(i+1)*cornerDim-1] = cv::Point2f(xCoord, vertex2.y);
+    }
+
+    // last column
+    corners[cornerDim*(cornerDim-1)] = cv::Point2f(vertex2.x, vertex1.y);
+    for(int i = 0; i < insideCornerDim; ++i){
+        corners[cornerDim*(cornerDim-1)+i+1] =  cv::Point2f(vertex2.x, insideCorners[i].y);
+    }
+    corners[corners.size()-1] = vertex2;
+
+    return std::vector<cv::Point2f>(corners);
+}
+
 std::vector<cv::Point2f> getBoardCorners(cv::Mat frame)
 {
     static int count = 0;
     static std::vector<cv::Point2f> result = std::vector<cv::Point2f>();
-    if (++count == 3) {
+    if (++count == 5) {
         count = 0;
         cv::Size patternsize(7, 7); //interior number of corners
         std::vector<cv::Point2f> tmp = std::vector<cv::Point2f>();
@@ -33,57 +79,8 @@ std::vector<cv::Point2f> getBoardCorners(cv::Mat frame)
         if(patternfound) {
             // improve accuracy
             cv::cornerSubPix(frame, tmp, cv::Size(11, 11), cv::Size(-1, -1), cv::TermCriteria(CV_TERMCRIT_EPS + CV_TERMCRIT_ITER, 30, 0.1));
-            std::vector<cv::Point2f> insideCorners = std::vector<cv::Point2f>(tmp);
-
-            int insideCornerDim = patternsize.width;
-
-            cv::Point2f vertex1 = cv::Point2f(
-                    insideCorners[0].x + abs(insideCorners[0].x - insideCorners[insideCornerDim].x),
-                    insideCorners[0].y - abs(insideCorners[0].y - insideCorners[1].y)
-                );
-
-            cv::Point2f vertex2 = cv::Point2f(
-                    insideCorners[insideCorners.size()-1].x - abs(insideCorners[insideCorners.size()-1].x - insideCorners[insideCorners.size()-1-insideCornerDim].x),
-                    insideCorners[insideCorners.size()-1].y + abs(insideCorners[insideCorners.size()-1].y - insideCorners[insideCorners.size()-2].y)
-                );
-
-            int cornerDim = insideCornerDim+2;
-            std::vector<cv::Point2f> corners  = std::vector<cv::Point2f>(cornerDim*cornerDim);
-
-            // first column
-
-            corners[0] = vertex1;
-
-            for(int i = 0; i < insideCornerDim; ++i){
-                corners[i+1] =  cv::Point2f( vertex1.x, insideCorners[i].y);
-            }
-
-            corners[cornerDim-1] = cv::Point2f(vertex1.x, vertex2.y);
-
-
-            // for every insideCorners' x coord
-            for(int i = 1; i < cornerDim -1 ; ++i){
-                int xCoord = insideCorners[(i-1)*insideCornerDim].x;
-                corners[i*cornerDim] = cv::Point2f( xCoord, vertex1.y);
-
-                //for every Corners' y coord
-                for(int j = 0;j < insideCornerDim;j++){
-                    corners[i*cornerDim+j+1] = cv::Point2f( xCoord, insideCorners[j].y);
-                }
-
-                corners[(i+1)*cornerDim-1] = cv::Point2f( xCoord, vertex2.y);
-
-            }
-
-            // last column
-            corners[cornerDim*(cornerDim-1)] = cv::Point2f( vertex2.x, vertex1.y );
-
-            for(int i = 0; i < insideCornerDim; ++i){
-            	corners[cornerDim*(cornerDim-1)+i+1] =  cv::Point2f(vertex2.x, insideCorners[i].y);
-            }
-
-            corners[corners.size()-1] = vertex2;
-            result = std::vector<cv::Point2f>(corners);
+            result = std::vector<cv::Point2f>(tmp);
+            result = completeBoardCorners(result);
         }
     }
 
@@ -99,22 +96,21 @@ void printVector(std::vector<cv::Point2f> v) {
 
 std::vector<cv::Point2f> getPositionCorners(std::vector<cv::Point2f> corners, int x, int y)
 {
+    int stride = BOARD_SIZE+1;
     std::vector<cv::Point2f> quad;
-    quad.push_back(corners.at(STRIDE*x + y));
-    quad.push_back(corners.at(STRIDE*x + (y + 1)));
-    quad.push_back(corners.at(STRIDE*(x + 1) + y));
-    quad.push_back(corners.at(STRIDE*(x + 1) + (y + 1)));
+    quad.push_back(corners.at(stride*x + y));
+    quad.push_back(corners.at(stride*x + (y + 1)));
+    quad.push_back(corners.at(stride*(x + 1) + y));
+    quad.push_back(corners.at(stride*(x + 1) + (y + 1)));
     return quad;
 }
 
 bool isPointInsideQuad(cv::Point2f c, std::vector<cv::Point2f> quad)
 {
     cv::Point2f p0 = quad.at(0);
-    cv::Point2f p1 = quad.at(1);
-    cv::Point2f p2 = quad.at(2);
     cv::Point2f p3 = quad.at(3);
     return p0.x <= c.x && c.x <= p3.x &&
-           p0.y <= c.y && c.y <= p3.x;
+           p0.y <= c.y && c.y <= p3.y;
 }
 
 int main(int argc, char* argv[])
@@ -133,7 +129,6 @@ int main(int argc, char* argv[])
     int gridx = 2;
     int gridy = 2;
     cv::namedWindow("grid", cv::WINDOW_NORMAL);
-    //cv::namedWindow("test", cv::WINDOW_NORMAL);
 
     while(true) {
         Game game;
@@ -151,6 +146,7 @@ int main(int argc, char* argv[])
         cv::Mat boardCorners = gray.clone();
         std::vector<cv::Point2f> corners = getBoardCorners(gray);
         cv::drawChessboardCorners(boardCorners, cv::Size(9, 9), cv::Mat(corners), !corners.empty());
+        printVector(corners);
         grid.push_back(boardCorners);
 
         // Threshold the HSV image, keep only the red pixels
@@ -170,11 +166,12 @@ int main(int argc, char* argv[])
             cv::Point2f center(round(circles[i][0]), round(circles[i][1]));
             int radius = round(circles[i][2]);
             cv::circle(redCircles, center, radius, cv::Scalar(0, 255, 0), 5);
-
             if (!corners.empty()) {
-                for (int x = 0; x < STRIDE; x++) {
-                    for (int y = 0; y < STRIDE; y++) {
+                for (int x = 0; x < BOARD_SIZE; x++) {
+                    for (int y = 0; y < BOARD_SIZE; y++) {
                         std::vector<cv::Point2f> quad = getPositionCorners(corners, x, y);
+                        std::cout << "(" << x << ", " << y << ") ";
+                        printVector(quad);
                         if (isPointInsideQuad(center, quad)) {
                             std::cout << std::endl << "Circle with center" << center << "is a piece that's inside game position (" << x << "," << y << ")" << std::endl;
                             game.set_red(x, y);
